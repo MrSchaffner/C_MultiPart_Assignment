b232574d2027a0d531b42710a47ecefdb348f820
GenoSophy <62560010+MrSchaffner@users.noreply.github.com>
2022-07-15 16:18:44 -0700

    Add project files.

---
+++ C_MultiPart_Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_MultiPart_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringies = { "flib", "bilf", "lifb" };
+            Console.WriteLine("Enter some text");
+            string inputText = Console.ReadLine();
+
+            for(int i=0; i<stringies.Length; i++)
+            {
+                stringies[i] = stringies[i] + " " + inputText;
+            }
+
+            foreach (string i in stringies)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
